b45e5256dc4080a830f00dfc81a8fa33db9e47c7
Sushant Raj <sushantraj215@gmail.com>
2023-10-29 01:26:05 +0530

    first

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const root = document.getElementById('root');
+
+const reactRoot = ReactDOM.createRoot(root);
+reactRoot.render(<App />);
